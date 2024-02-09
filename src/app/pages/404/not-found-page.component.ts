0f9327d2ad94245bfea32c5666d355afd29ea6c4
Anatoly Mishin <lim0ncmon@yandex.ru>
2024-02-09 10:35:35 +0300

    404

--- src/app/pages/404/not-found-page.component.ts
+++ src/app/pages/404/not-found-page.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'autodoc-not-found-page',
   templateUrl: './not-found-page.component.html',
-  styleUrl: './not-found-page.component.scss'
+  styleUrl: './not-found-page.component.scss',
+  host: { class: 'not-found' },
+  encapsulation: ViewEncapsulation.None
 })
 export class NotFoundPageComponent {
 
